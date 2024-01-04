e0f960c4c116833c257f047b8fa8f0ee7f142253
minju <betaa0528@gmail.com>
2024-01-04 16:27:32 +0900

    ProxyPattern 공부 디자인 패턴 중 ProxyPattern에 대해 작성해 보았다.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,62 @@
 import adpter.*;
+import aop.AopBrowser;
+import proxy.Browser;
+import proxy.BrowserProxy;
+import proxy.IBrowser;
 
 import javax.swing.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
-        HairDryer hairDryer = new HairDryer();
-        connect(hairDryer);
+//        HairDryer hairDryer = new HairDryer();
+//        connect(hairDryer);
+//
+//        Cleaner cleaner = new Cleaner();
+//        Electronic110V adapter = new SocketAdapter(cleaner);
+//        connect(adapter);
+//
+//        AirConditioner airConditioner = new AirConditioner();
+//        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+//        connect(airAdapter);
 
-        Cleaner cleaner = new Cleaner();
-        Electronic110V adapter = new SocketAdapter(cleaner);
-        connect(adapter);
+//        Browser browser = new Browser("www.naver.com");
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
 
-        AirConditioner airConditioner = new AirConditioner();
-        Electronic110V airAdapter = new SocketAdapter(airConditioner);
-        connect(airAdapter);
+//        IBrowser browser = new BrowserProxy("www.naver.com");
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
+
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                () -> {
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                () -> {
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
     }
 
     //콘센트
-    public static void connect(Electronic110V electronic110V){
+    public static void connect(Electronic110V electronic110V) {
         electronic110V.powerOn();
     }
 }
